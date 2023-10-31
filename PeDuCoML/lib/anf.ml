046616f0847142d82163d484a301425f4898005c
Danila-Pechenev <danil-pechenev@mail.ru>
2023-10-31 18:15:17 +0300

    Rewrite ANF totally

--- PeDuCoML/lib/anf.ml
+++ PeDuCoML/lib/anf.ml
@@ -1,4 +1,5 @@
 open Ast
+open Util
 
 type unique_id = int [@@deriving eq, show { with_path = false }]
 
@@ -8,7 +9,6 @@ type imm_expr =
   | ImmChar of char
   | ImmBool of bool
   | ImmUnit
-  | ImmWildcard
   | ImmId of unique_id
 
 type cexpr =
@@ -19,24 +19,16 @@ type cexpr =
   | CTuple of imm_expr list
   | CIf of imm_expr * imm_expr * imm_expr
   | CConstructList of imm_expr * imm_expr
-  | CFun of pattern list * imm_expr
-  | CMatchWith of imm_expr * (pattern * imm_expr) list
+  | CMatchWith of imm_expr * (pattern * aexpr) list
   | CImm of imm_expr
 
-and cpatt =
-  | CPImm of cexpr
-  | CPWildcard
-  | CPTuple of cpatt list
-  | CPList of pattern list
-  | CPConstructList of pattern * pattern
-
-type aexpr =
+and aexpr =
   | ALet of unique_id * cexpr * aexpr
   | ACExpr of cexpr
 
-type let_fun =
-  | FunctionCarry of cexpr * let_fun
-  | Function of cexpr * cexpr
+type global_scope_function =
+  | FunctionWithArgs of (imm_expr -> global_scope_function)
+  | FunctionNoArgs of aexpr
 
 (* Smart constructors *)
 (* imm_expr *)
@@ -45,7 +37,6 @@ let imm_string str = ImmString str
 let imm_char sym = ImmChar sym
 let imm_bool b = ImmBool b
 let imm_unit = ImmUnit
-let imm_wildcard = ImmWildcard
 let imm_id id = ImmId id
 
 (* cexpr *)
@@ -57,7 +48,6 @@ let clist imm_expr_list = CList imm_expr_list
 let ctuple imm_expr_list = CTuple imm_expr_list
 let cif condition true_branch false_branch = CIf (condition, true_branch, false_branch)
 let cconstruct_list operand imm_expr_list = CConstructList (operand, imm_expr_list)
-let cfun imm_expr_list imm_expr = CFun (imm_expr_list, imm_expr)
 let cmatch_with imm_expr case_list = CMatchWith (imm_expr, case_list)
 
 (* aexpr *)
@@ -101,10 +91,7 @@ end
 
 open AnfConvert
 
-let rec expression_anf
-  (env : (string, 'a, Base.String.comparator_witness) Base.Map.t)
-  expr
-  k
+let rec anf (env : (string, 'a, Base.String.comparator_witness) Base.Map.t) expr k
   : aexpr AnfConvert.t
   =
   match expr with
@@ -115,88 +102,89 @@ let rec expression_anf
      | LChar sym -> k (imm_char sym)
      | LBool b -> k (imm_bool b)
      | LUnit -> k imm_unit)
-  | EIdentifier x ->
-    (match Base.Map.find env x with
-     | Some id -> k @@ imm_id id
-     | _ -> failwith "Not reachable. If it is, kill Danya and Ilya")
+  | EIdentifier x -> k @@ imm_id (Base.Map.find_exn env x)
   | EBinaryOperation (bop, left, right) ->
     let* fresh_var = fresh in
-    expression_anf env left (fun limm ->
-      expression_anf env right (fun rimm ->
-        let* body = k @@ imm_id fresh_var in
+    let* body = k @@ imm_id fresh_var in
+    anf env left (fun limm ->
+      anf env right (fun rimm ->
         return @@ alet fresh_var (cbinary_operation bop limm rimm) body))
   | EUnaryOperation (uop, expr) ->
     let* fresh_var = fresh in
-    expression_anf env expr (fun imm ->
-      let* body = k @@ imm_id fresh_var in
-      return @@ alet fresh_var (cunary_operation uop imm) body)
+    let* body = k @@ imm_id fresh_var in
+    anf env expr (fun imm -> return @@ alet fresh_var (cunary_operation uop imm) body)
   | EApplication (fun_expr, arg_expr) ->
     let* fresh_var = fresh in
-    expression_anf env fun_expr (fun imm_fun_expr ->
-      expression_anf env arg_expr (fun imm_arg_expr ->
-        let* body = k @@ imm_id fresh_var in
+    let* body = k @@ imm_id fresh_var in
+    anf env fun_expr (fun imm_fun_expr ->
+      anf env arg_expr (fun imm_arg_expr ->
         return @@ alet fresh_var (capplication imm_fun_expr imm_arg_expr) body))
   | EList expr_list ->
     let* fresh_var = fresh in
     let* body = k @@ imm_id fresh_var in
     let rec helper curr_list = function
-      | head :: tail ->
-        expression_anf env head (fun imm -> helper (imm :: curr_list) tail)
+      | head :: tail -> anf env head (fun imm -> helper (imm :: curr_list) tail)
       | _ -> return @@ alet fresh_var (clist @@ List.rev curr_list) body
     in
     helper [] expr_list
-  | ETuple expr_list ->
+  | ETuple (first_elem, second_elem, other_elems) ->
+    let all_elems = first_elem :: second_elem :: other_elems in
     let* fresh_var = fresh in
     let* body = k @@ imm_id fresh_var in
     let rec helper curr_list = function
-      | head :: tail ->
-        expression_anf env head (fun imm -> helper (imm :: curr_list) tail)
+      | head :: tail -> anf env head (fun imm -> helper (imm :: curr_list) tail)
       | _ -> return @@ alet fresh_var (ctuple @@ List.rev curr_list) body
     in
-    helper [] expr_list
+    helper [] all_elems
   | EIf (condition, true_branch, false_branch) ->
     let* fresh_var = fresh in
-    expression_anf env condition (fun condition_imm ->
-      expression_anf env true_branch (fun true_branch_imm ->
-        expression_anf env false_branch (fun false_branch_imm ->
-          let* body = k @@ imm_id fresh_var in
+    let* body = k @@ imm_id fresh_var in
+    anf env condition (fun condition_imm ->
+      anf env true_branch (fun true_branch_imm ->
+        anf env false_branch (fun false_branch_imm ->
           return
           @@ alet fresh_var (cif condition_imm true_branch_imm false_branch_imm) body)))
   | EConstructList (operand, expr_list) ->
     let* fresh_var = fresh in
-    expression_anf env operand (fun operand_imm ->
-      expression_anf env expr_list (fun expr_list_imm ->
-        let* body = k @@ imm_id fresh_var in
-        return @@ alet fresh_var (cconstruct_list operand_imm expr_list_imm) body))
-  | ELetIn (bindings_list, expr) ->
-    let* fresh_var = fresh in
-    (match bindings_list with
-     | ( DDeclaration (name, pattern_list, expr)
-       | DRecursiveDeclaration (name, pattern_list, expr) )
-       :: tail ->
-       let env = Base.Map.update env name ~f:(fun _ -> fresh_var) in
-       expression_anf env (efun pattern_list expr) (fun imm ->
-         let* body = expression_anf env (eletin tail expr) k in
-         return @@ alet fresh_var (cimm imm) body)
-     | _ -> expression_anf env expr k)
-  | EFun (arguments, expr) ->
-    let* fresh_var = fresh in
     let* body = k @@ imm_id fresh_var in
-    let update_map env patt =
-      let identifiers = Util.find_identifiers_pattern patt in
-      Base.List.fold_right identifiers ~init:(return env) ~f:(fun id acc ->
-        let* fresh_var = fresh in
-        let* acc = acc in
-        return @@ Base.Map.update acc id ~f:(fun _ -> fresh_var))
-    in
-    let* env =
-      Base.List.fold_right arguments ~init:(return env) ~f:(fun patt acc ->
-        let* acc = acc in
-        update_map acc patt)
-    in
-    expression_anf env expr (fun imm ->
-      return @@ alet fresh_var (cfun arguments imm) body)
-  | EMatchWith (matched_expression, case_list) ->
+    anf env operand (fun operand_imm ->
+      anf env expr_list (fun expr_list_imm ->
+        return @@ alet fresh_var (cconstruct_list operand_imm expr_list_imm) body))
+  | ELetIn (first_declaration, other_declarations, body) ->
+    let* fresh_var = fresh in
+    (match first_declaration with
+     | DDeclaration (name, pattern_list, expr) ->
+       if List.length pattern_list = 0
+       then (
+         let new_env = Base.Map.update env name ~f:(fun _ -> fresh_var) in
+         anf env expr (fun imm_expr ->
+           match other_declarations with
+           | head :: tail ->
+             let* body = anf new_env (eletin head tail body) k in
+             return @@ alet fresh_var (cimm imm_expr) body
+           | _ ->
+             let* body = anf new_env body k in
+             return @@ alet fresh_var (cimm imm_expr) body))
+       else
+         failwith
+           "AST to ANF convertion error: internal let-expressions must not have any args."
+     | DRecursiveDeclaration (name, pattern_list, expr) ->
+       if List.length pattern_list = 0
+       then (
+         let env = Base.Map.update env name ~f:(fun _ -> fresh_var) in
+         anf env expr (fun imm_expr ->
+           match other_declarations with
+           | head :: tail ->
+             let* body = anf env (eletin head tail body) k in
+             return @@ alet fresh_var (cimm imm_expr) body
+           | _ ->
+             let* body = anf env body k in
+             return @@ alet fresh_var (cimm imm_expr) body))
+       else
+         failwith
+           "AST to ANF convertion error: internal let-expressions must not have any args.")
+  | EMatchWith (matched_expression, first_case, other_cases) ->
+    let case_list = first_case :: other_cases in
     let* fresh_var = fresh in
     let* body = k @@ imm_id fresh_var in
     let update_map env patt =
@@ -209,27 +197,53 @@ let rec expression_anf
     let rec helper imm_matched env curr_list = function
       | head :: tail ->
         let* new_env = update_map env (fst head) in
-        expression_anf new_env (snd head) (fun imm ->
-          helper imm_matched env ((fst head, imm) :: curr_list) tail)
+        let* case_anf = anf new_env (snd head) (fun imm -> return @@ acimm imm) in
+        helper imm_matched env ((fst head, case_anf) :: curr_list) tail
       | _ -> return @@ alet fresh_var (cmatch_with imm_matched @@ List.rev curr_list) body
     in
-    expression_anf env matched_expression (fun imm_matched ->
-      helper imm_matched env [] case_list)
+    anf env matched_expression (fun imm_matched -> helper imm_matched env [] case_list)
+  | EFun _ ->
+    failwith
+      "AST to ANF convertion error: EFun constructor cannot be used in the program after \
+       closure conversion and lambda lifting."
 ;;
 
-let declaration_anf env = function
-  | DDeclaration (name, pattern_list, expr)
+let process_declaration env declaration =
+  let find_all_pattern_ids pattern_list =
+    let rec helper current_list = function
+      | head :: tail -> helper (current_list @ find_identifiers_pattern head) tail
+      | _ -> current_list
+    in
+    helper [] pattern_list
+  in
+  let update_map env all_pattern_ids =
+    Base.List.fold_right all_pattern_ids ~init:(return env) ~f:(fun id acc ->
+      let* fresh_var = fresh in
+      let* acc = acc in
+      return @@ Base.Map.update acc id ~f:(fun _ -> fresh_var))
+  in
+  let get_function env pattern_list expr =
+    let* env = update_map env @@ find_all_pattern_ids pattern_list in
+    let* function_body_aexpr = anf env expr (fun imm -> return @@ acimm imm) in
+    let rec helper current_aexpr = function
+      | head :: tail ->
+        FunctionWithArgs
+          (fun imm -> helper (acexpr @@ cmatch_with imm [ head, current_aexpr ]) tail)
+      | _ -> FunctionNoArgs current_aexpr
+    in
+    return @@ helper function_body_aexpr pattern_list
+  in
+  let* fresh_var = fresh in
+  match declaration with
+  | DDeclaration (name, pattern_list, expr) ->
+    return @@ (name, fresh_var, get_function env pattern_list expr)
   | DRecursiveDeclaration (name, pattern_list, expr) ->
-    let* fresh_var = fresh in
-    let env = Base.Map.set env ~key:name ~data:fresh_var in
-    (match pattern_list with
-     | [] ->
-       expression_anf env expr (fun imm ->
-         return @@ alet fresh_var (cimm imm) (acimm (imm_id fresh_var)))
-     | _ -> expression_anf env (efun pattern_list expr) (fun imm -> return @@ acimm imm))
+    let env = Base.Map.update env name ~f:(fun _ -> fresh_var) in
+    return @@ (name, fresh_var, get_function env pattern_list expr)
 ;;
 
-let convert (program : declaration list) =
+(* convert and run_convert -- TODO (on Danya) *)
+(* let convert (program : declaration list) =
   let rec helper env = function
     | head :: tail ->
       let* head_declaration = declaration_anf env head in
@@ -245,16 +259,4 @@ let convert (program : declaration list) =
   helper (Base.Map.empty (module Base.String)) program
 ;;
 
-let run_convert program = run @@ convert program
-(*
-let%test _ =
-  run_convert @@ EBinaryOperation (Div, ELiteral (LInt 4), ELiteral (LInt 2))
-  = ALet (1, CBinaryOperation (Div, ImmInt 4, ImmInt 2), ACExpr (CImm (ImmId 1)))
-;;
-
-let%test _ =
-  run_convert
-  @@ EBinaryOperation
-       (Add, EIdentifier "x", EBinaryOperation (Sub, EIdentifier "x", EIdentifier "y"))
-  = ALet (1, CBinaryOperation (Div, ImmInt 4, ImmInt 2), ACExpr (CImm (ImmId 1)))
-;; *)
+let run_convert program = run @@ convert program *)

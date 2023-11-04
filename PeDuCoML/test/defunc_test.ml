38539acc9d39d3d6bc26e4dae12c2d221177f8bd
Ilya Dudnikov <dudnikoff98@gmail.com>
2023-11-04 15:17:08 +0300

    Implement test template for lambda lifting

--- PeDuCoML/test/defunc_test.ml
+++ PeDuCoML/test/defunc_test.ml
@@ -2,13 +2,78 @@ open PeDuCoML.Parser
 open PeDuCoML.Pprintast
 open PeDuCoML.Closure_conversion
 open PeDuCoML.Lambda_lift
+open PeDuCoML.Ast
+
+type func =
+  | FFun of expression
+  | FLet of declaration
+
+let ffun expr = FFun expr
+let flet decl = FLet decl
+
+let check_nested_functions =
+  let rec check_decl depth failed = function
+    | (DDeclaration (_, _, body) | DRecursiveDeclaration (_, _, body)) as original ->
+      let failed = if depth > 0 then flet original :: failed else failed in
+      check_expr (depth + 1) failed body
+  and check_expr depth failed =
+    let next_depth = depth + 1 in
+    function
+    | EBinaryOperation (_, left, right) ->
+      check_expr next_depth (check_expr next_depth failed left) right
+    | EUnaryOperation (_, operand) -> check_expr next_depth failed operand
+    | EApplication (func, arg) ->
+      check_expr next_depth (check_expr next_depth failed func) arg
+    | EFun (_, _, body) as original ->
+      let failed = if depth > 0 then ffun original :: failed else failed in
+      check_expr next_depth failed body
+    | EList expr_list ->
+      (match expr_list with
+       | [] -> failed
+       | head :: tail ->
+         check_expr next_depth (check_expr next_depth failed head) (elist tail))
+    | EConstructList (head, tail) ->
+      check_expr next_depth (check_expr next_depth failed head) tail
+    | ETuple (first_elem, second_elem, other_elems) ->
+      check_expr depth failed (elist @@ (first_elem :: second_elem :: other_elems))
+    | ELetIn (first_decl, other_decls, body) ->
+      let failed = check_decl depth failed first_decl in
+      (match other_decls with
+       | head :: tail -> check_expr next_depth failed (eletin head tail body)
+       | [] -> check_expr next_depth failed body)
+    | EIf (condition, true_branch, false_branch) ->
+      let failed = check_expr next_depth failed condition in
+      check_expr next_depth (check_expr next_depth failed true_branch) false_branch
+    | EMatchWith (matched, (_, action), other_cases) ->
+      let failed = check_expr next_depth failed action in
+      (match other_cases with
+       | head :: tail -> check_expr next_depth failed (ematchwith matched head tail)
+       | [] -> check_expr next_depth failed matched)
+    | _ -> failed
+  in
+  let rec helper acc = function
+    | [] -> acc
+    | head :: tail ->
+      (match head with
+       | DDeclaration (_, _, body) | DRecursiveDeclaration (_, _, body) ->
+         helper (check_expr 0 [] body) tail)
+  in
+  helper []
+;;
+
+let pp_func fmt = function
+  | FFun expr -> pp_expression fmt expr
+  | FLet decl -> pp_declaration fmt decl
+;;
 
 let print_anf code =
   match parse code with
   | Ok ast ->
     let closure = run_closure ast in
     let defunced = run_lambda_lifting closure in
-    List.iter (fun decl -> Format.printf "%a\n" pp_declaration decl) defunced
+    (match check_nested_functions defunced with
+     | [] -> Format.printf "Ok!\n"
+     | failed -> List.iter (fun func -> Format.printf "%a\n" pp_func func) failed)
   | Error err -> Format.printf "%s\n" err
 ;;
 
--- PeDuCoML/test/defunc_test.t
+++ PeDuCoML/test/defunc_test.t
@@ -1,28 +1,24 @@
   $ ./defunc_test.exe <<- EOF
   > let main = fun x -> x
   > EOF
-  let `ll_0 x = x
-  let main  = `ll_0
+  Ok!
   $ ./defunc_test.exe <<- EOF
   > let main x = 
   >   let const f = fun s -> f in
   >   let rev_const f s = const s in
   >   rev_const (fun _ -> x)
   > EOF
-
+  Ok!
   $ ./defunc_test.exe <<- EOF
   > let main = 
   >   let const = fun f s -> f in
   >   const
   > EOF
-
-  $ ./defunc_test.exe <<- EOF
-  > let `ll_0 x = x
-  > let main  = `ll_0
-
+  Ok!
   $ ./defunc_test.exe <<- EOF
   > let main x = 
   >   let const f = fun s -> f in
   >   let rev_const f s = const s in
   >   rev_const (fun _ -> x)
   > EOF
+  Ok!

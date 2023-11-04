a604584f66fb47367d11a089c39bf7eb6d396052
Ilya Dudnikov <dudnikoff98@gmail.com>
2023-11-04 16:17:52 +0300

    Fix utils

--- PeDuCoML/lib/util.ml
+++ PeDuCoML/lib/util.ml
@@ -1,35 +1,11 @@
 open Ast
-open Base.Set
 
-let empty = empty (module Base.String)
-let singleton = singleton (module Base.String)
-
-(** Finds all identifiers in an expression. Used in pattern-matching inference *)
-let rec find_identifiers = function
-  | EBinaryOperation (_, left_operand, right_operand) ->
-    union (find_identifiers left_operand) (find_identifiers right_operand)
-  | EUnaryOperation (_, operand) -> find_identifiers operand
-  | EApplication (left_operand, right_operand) ->
-    union (find_identifiers left_operand) (find_identifiers right_operand)
-  | EIdentifier id -> singleton id
-  | EList expression_list ->
-    Base.List.fold_right
-      ~f:(fun expression acc -> union (find_identifiers expression) acc)
-      ~init:empty
-      expression_list
-  | ETuple (first_elem, second_elem, other_elems) ->
-    Base.List.fold_right
-      ~f:(fun expression acc -> union (find_identifiers expression) acc)
-      ~init:empty
-    @@ (first_elem :: second_elem :: other_elems)
-  | EConstructList (operand, list) ->
-    union (find_identifiers operand) (find_identifiers list)
-  | _ -> empty
-;;
+let empty = Base.Set.empty (module Base.String)
+let singleton = Base.Set.singleton (module Base.String)
 
 let find_identifiers_pattern =
   let rec helper acc = function
-    | PIdentifier id -> add acc id
+    | PIdentifier id -> Base.Set.add acc id
     | PList pattern_list ->
       (match pattern_list with
        | head :: tail -> helper (helper acc head) (PList tail)
@@ -37,6 +13,56 @@ let find_identifiers_pattern =
     | PTuple (first_pattern, second_pattern, other_patterns) ->
       helper acc @@ plist (first_pattern :: second_pattern :: other_patterns)
     | PConstructList (operand, list) -> helper (helper acc operand) list
+    | _ -> acc
+  in
+  helper empty
+;;
+
+(** Finds all identifiers in an expression. Used in pattern-matching inference *)
+let find_identifiers =
+  let rec decl_helper acc = function
+    | DDeclaration (_, pattern_list, body) | DRecursiveDeclaration (_, pattern_list, body)
+      ->
+      let pattern_args =
+        Base.List.fold_right
+          pattern_list
+          ~f:(fun pattern acc -> Base.Set.union acc (find_identifiers_pattern pattern))
+          ~init:acc
+      in
+      Base.Set.diff (helper acc body) pattern_args
+  and helper acc = function
+    | EBinaryOperation (_, left, right) -> helper (helper acc left) right
+    | EUnaryOperation (_, operand) -> helper acc operand
+    | EApplication (func, arg) -> helper (helper acc func) arg
+    | EIdentifier id -> Base.Set.add acc id
+    | EFun (first_arg, other_args, body) ->
+      let pattern_args =
+        Base.List.fold_right
+          (first_arg :: other_args)
+          ~f:(fun pattern acc -> Base.Set.union acc (find_identifiers_pattern pattern))
+          ~init:acc
+      in
+      Base.Set.diff (helper acc body) pattern_args
+    | EList expr_list ->
+      (match expr_list with
+       | head :: tail -> helper (helper acc head) (elist tail)
+       | [] -> acc)
+    | EConstructList (head, tail) -> helper (helper acc head) tail
+    | ETuple (first_elem, second_elem, other_elems) ->
+      helper acc (elist @@ (first_elem :: second_elem :: other_elems))
+    | ELetIn (first_decl, other_decls, body) ->
+      let acc = decl_helper acc first_decl in
+      (match other_decls with
+       | head :: tail -> helper acc (eletin head tail body)
+       | [] -> helper acc body)
+    | EIf (condition, true_branch, false_branch) ->
+      let acc = helper acc condition in
+      helper (helper acc true_branch) false_branch
+    | EMatchWith (matched, (pattern, action), other_cases) ->
+      let acc = Base.Set.diff (find_identifiers_pattern pattern) (helper acc action) in
+      (match other_cases with
+       | head :: tail -> helper acc (ematchwith matched head tail)
+       | [] -> helper acc matched)
     | _ -> acc
   in
   helper empty
